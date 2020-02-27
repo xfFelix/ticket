63a8cc0ccaa9a10c36559aa9647497946c76eeaa
xfFelix <13590344852@163.com>
2020-02-27 14:31:48 +0800

    删掉ticket

--- src/router/hook/baiduStatis.js
+++ src/router/hook/baiduStatis.js
@@ -3,8 +3,9 @@ export default router => {
   window._hmt = _hmt; // 必须把_hmt挂载到window下，否则找不到
   router.beforeEach((to, from, next) => {
     if (_hmt) {
+      console.log(_hmt)
       if (to.path) {
-        _hmt.push(['_trackPageview', '/ticket' + to.fullPath])
+        _hmt.push(['_trackPageview', to.fullPath])
       }
     }
     next();
