f41c62e0711fd2aa9978033c798376e109ec6a40
xfFelix <13590344852@163.com>
2020-07-22 19:20:29 +0800

    修改样式

--- cert/src/views/sign_com/signHelper/components/personInfo.vue
+++ cert/src/views/sign_com/signHelper/components/personInfo.vue
@@ -34,7 +34,7 @@
       </li>
       <li>
         <span>手机验证码</span>
-        <input type="text" v-model.trim="data.code"/>
+        <input type="tel" v-model.trim="data.code" :maxlength="4" pattern="[0-9]*"/>
         <button @click.prevent="sendCode" :disabled="codeFlag" class="bnt" type="button">{{codeText}}</button>
       </li>
     </ul>
