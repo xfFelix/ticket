9425a0ef855ec9fa02d6eba8d1507716bb75a281
xfFelix <13590344852@163.com>
2020-09-25 15:49:03 +0800

    修改人脸识别

--- cert/src/views/face/user/index.vue
+++ cert/src/views/face/user/index.vue
@@ -8,11 +8,25 @@
       </div>
       <div class="input-item">
         <div class="pre">真实姓名</div>
-        <input type="text" placeholder="请输入姓名" v-model="name">
+        <input type="text" placeholder="请输入姓名" v-model.trim="name">
       </div>
       <div class="input-item">
         <div class="pre">证件号码</div>
-        <input type="text" placeholder="请输入证件号码" v-model="idcard">
+        <input type="text" placeholder="请输入证件号码" v-model.trim="idcard">
+      </div>
+      <div class="input-item">
+        <div class="pre">手机号</div>
+        <input type="tel" placeholder="请输入联系方式" pattern="[0-9]*" :maxlength="11" v-model.trim="mobile">
+      </div>
+      <div class="input-item">
+        <div class="pre">图形验证码</div>
+        <input type="text" placeholder="请输入图形验证码" :maxlength="4" v-model.trim="verify">
+        <img :src="validateImgSrc" class="img_captcha" @click="validateImgClick()">
+      </div>
+      <div class="input-item">
+        <div class="pre">手机验证码</div>
+        <input type="tel" placeholder="请输入手机验证码" pattern="[0-9]*" :maxlength="4" v-model.trim="code">
+        <button @click.prevent="sendCode" :disabled="codeFlag" class="bnt" type="button">{{codeText}}</button>
       </div>
       <button class="btn-next" @click="validate">下一步</button>
       <div class="hint">
@@ -24,16 +38,22 @@
 </template>
 <script>
 import { mapActions } from 'vuex'
-import { getArgs } from '@/util/common'
+import { getArgs, IsMobile, isEmpty } from '@/util/common'
 import Loading from '@/util/loading'
 export default {
   data: () => ({
     name: '',
     idcard: '',
+    mobile: '',
+    verify: '',
+    code: '',
     type: '',
     uid: '',
     url: '',
-    jumpPage: ''
+    jumpPage: '',
+    validateImgSrc: '',
+    codeText: "发送验证码",
+    codeFlag: false,
   }),
   created () {
     const { type, uid, url, jumpPage } = getArgs()
@@ -43,6 +63,7 @@ export default {
     this.uid = uid
     this.url = url
     this.jumpPage = jumpPage
+    this.validateImgClick()
   },
   methods: {
     ...mapActions({
@@ -51,6 +72,9 @@ export default {
     validate() {
       if (!this.name) return this.$toast('请输入姓名')
       if (!this.idcard) return this.$toast('请输入证件号码')
+      if (!IsMobile(this.mobile)) return this.$toast("请输入正确的手机号");
+      if (isEmpty(this.verify)) return this.$toast("请输入图形验证码");
+      if (isEmpty(this.code)) return this.$toast("请输入手机验证码")
       this.goNext()
     },
     async goNext() {
@@ -58,7 +82,8 @@ export default {
       try {
         loading.show()
         const { checkInfoByFace } = await import('api')
-        const { data, code, msg } = await checkInfoByFace({idNo: this.idcard, name: this.name, type: this.type, uid: this.uid, url: this.url, jumpPage: this.jumpPage})
+        const { mobile, idcard, name, type, uid, url, jumpPage } = this
+        const { data, code, msg } = await checkInfoByFace({idNo: idcard, name, type, uid, url, jumpPage, mobile, code: this.code})
         if (+code === 2222) {
           loading.hide()
           this.setConfig({name: this.name, idcard: this.idcard, accountId: data})
@@ -70,7 +95,37 @@ export default {
       } catch (e) {
         this.$toast(e)
       }
-    }
+    },
+    validateImgClick(){
+      this.validateImgSrc = process.env.VUE_APP_CONTRACT_URL + '/contract/captcha?' + (new Date());
+    },
+    async sendCode() {
+      if (!IsMobile(this.mobile)) return this.$toast("请输入正确的手机号");
+      if (isEmpty(this.verify)) return this.$toast("请输入图形验证码");
+      try{
+        const { contractSms } = await import(/* webpackPrefetch: true */ "api");
+        let data = await contractSms({ mobile: this.mobile,code:this.verify});
+      }catch (e) {
+        this.validateImgClick();
+        this.verify = '';
+        return this.$toast(e);
+      }
+      this.codeText = "120s 重新获取";
+      let _this = this;
+      let timeInit = 120;
+      let countDown = window.setInterval(function() {
+        let i = 1;
+        timeInit = timeInit - i;
+        if (timeInit > 0) {
+          _this.codeText = timeInit + "s 重新获取";
+          _this.codeFlag = true;
+        } else {
+          _this.codeText = "重新获取";
+          _this.codeFlag = false;
+          window.clearInterval(countDown);
+        }
+      }, 1000);
+    },
   }
 }
 </script>
@@ -112,6 +167,7 @@ export default {
       align-items: center;
       border-bottom: 1px solid rgba(246,246,246,1);
       font-size: 15px;
+      position: relative;
       .pre{
         flex: 0 0 100px;
         color: #666666;
@@ -124,6 +180,24 @@ export default {
         &::placeholder{
           color: #999;
         }
+      }
+      .img_captcha{
+        position: absolute;
+        right: 0;
+        width: 90px;
+        height: 40px;
+      }
+      .bnt{
+        text-align: center;
+        height: 28px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid #30ce84;
+        color: #30ce84;
+        background: #fff;
+        position: absolute;
+        right: 0;
       }
     }
     .btn-next{
